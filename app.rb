838290b58e4a340dfbda144cb31a4095d2141a5d
KavyaSek <ksekar@chicagobooth.edu>
2024-10-23 19:16:45 +0000

    AA

--- app.rb
+++ app.rb
@@ -12,18 +12,38 @@ end
 get("/squares/results") do
   number = params[:number]
   @num = number.to_i
-  @result = @num * @num
+  @result = (@num * @num).to_f
   erb(:squares_res)
 end
- 
 
 get("/square_root/new") do
   erb(:square_root)
 end
 
+get("/square_root/results") do
+  number = params[:number]
+  @num = number.to_i
+  @result = Math.sqrt(@num)
+  erb(:square_root_res)
+end
+
 get("/payment/new") do
   erb(:payment)
 end
+
+get("/payment/results") do
+  @apr = params[:apr]
+  @years = params[:years]
+  @principal = params[:principal]
+  r = @apr.to_f/(100*12)
+  n = @years.to_i*12
+  pv = @principal.to_i
+  @numerator = r*pv
+  @denominator = (1+r)**(-n)
+  @pmt = @numerator/@denominator
+  erb(:payment_res)
+end
+
 
 get("/random/new") do
   erb(:random)
