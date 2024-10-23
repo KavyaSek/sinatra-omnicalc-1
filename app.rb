06748de339137ad36f6685f7004ea415dea54bf4
KavyaSek <ksekar@chicagobooth.edu>
2024-10-23 16:37:26 +0000

    AA

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:squares)
+end
+
+get("/square/new") do
+   erb(:squares)
+end
+
+get("/square/results") do
+  number = params[:number]
+  @num = number
+  @result = @num * @num
+  erb(:squares_res)
+end
+ 
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/random/new") do
+  erb(:random)
 end
